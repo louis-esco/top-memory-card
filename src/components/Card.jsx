0e50e6ce65c21e5cbdeebea64f8fb45a28593a74
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-12 16:46:29 +0200

    Adds front and back card side and flip transition

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,28 @@
+import { useEffect } from "react";
 import "../styles/card.css";
 
-export default function Card({ pokeName, pokeId, spriteUrl, handleClick }) {
+export default function Card({
+  pokeName,
+  pokeId,
+  spriteUrl,
+  handleClick,
+  flipCard,
+}) {
   return (
     <>
-      <button className="poke-card-btn" id={pokeId} onClick={handleClick}>
-        <div className="poke-img">
-          <img src={spriteUrl} alt={`Sprite of ${pokeName}`} />
+      <div className={flipCard ? "poke-card flip" : "poke-card"}>
+        <button className="card-front" id={pokeId} onClick={handleClick}>
+          <div className="poke-img">
+            <img src={spriteUrl} alt={`Sprite of ${pokeName}`} />
+          </div>
+          <div className="poke-name">
+            {pokeName.charAt(0).toUpperCase() + pokeName.slice(1)}
+          </div>
+        </button>
+        <div className="card-back">
+          <img src="/src/assets/pokeball.png" alt="" />
         </div>
-        <div className="poke-name">
-          {pokeName.charAt(0).toUpperCase() + pokeName.slice(1)}
-        </div>
-      </button>
+      </div>
     </>
   );
 }
